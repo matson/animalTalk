fb08399830429ff1f44388e011d635a846ecce02
Madison <madison.k.adams@gmail.com>
2023-05-03 13:07:30 -0400

    code

--- animalTalks/ViewController.swift
+++ animalTalks/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func lionButton(_ sender: UIButton) {
+        print("The lion")
+        
+    }
+    
+    func playSound() {
+         let url = Bundle.main.url(forResource: "soundName", withExtension: "wav")
+         player = try! AVAudioPlayer(contentsOf: url!)
+         player.play()
+    }
+       
+    
+    @IBAction func monkeyButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func penguinButton(_ sender: UIButton) {
+        
+        print("The penguin")
+    }
+    
+      
+    
+    @IBAction func snakeButton(_ sender: UIButton) {
+    }
+  
+    
+    @IBAction func wolfButton(_ sender: UIButton) {
+    }
+    
+    
 }
 
